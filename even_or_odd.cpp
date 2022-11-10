9937f3193fd91132cf02d6ec7100c55671277800
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-11-10 01:50:43 +0000

    update

---
+++ even_or_odd.cpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2022 Joanne Santhosh
+//
+// Created by: Joanne Santhosh
+// Created on: Nov 2022
+// This program uses while loops
+
+#include <iostream>
+#include <string>
+
+
+int main() {
+    // Variables
+    std::string integerAsString;
+    int integer;
+
+    // Input
+    std::cout << "Enter an integer: ";
+    std::cin >> integerAsString;
+
+    // Process and Output
+    try {
+        integer = std::stoi(integerAsString);
+        if (integer % 2 == 0) {
+            std::cout << integer << " is an even number ";
+        } else {
+            std::cout << integer << " is an odd number.";
+        }
+    } catch (std::invalid_argument) {
+        std::cout << "Error: " << integerAsString << " is not an integer.";
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
